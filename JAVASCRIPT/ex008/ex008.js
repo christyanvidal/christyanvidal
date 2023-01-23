fa61ef1af4d17b08ed7c4bf52410357207c3d4af
Christyan Vidal <120426424+christyanvidal@users.noreply.github.com>
2023-01-23 12:32:45 -0300

    Exercícios de JS atualizados

---
+++ JAVASCRIPT/ex008/ex008.js
@@ -0,0 +1,6 @@
+ console.log('O console funcionou corretamente')
+ var velocidade = 112.3
+ console.log(`A velocidade do seu carro é ${velocidade} km/h`)
+ if ( velocidade > 60) {console.log (`Você Vacilou e ultrapassou os 60 km/h e será multado`)}
+ if ( velocidade > 100) {console.log(`Você ultapassou a velocidade de 100 km/h e isso é uma infração multo grave ! Será multado!`)}
+ console.log(`Dirija sempre com cinto de segurança , e se beber não dirija !`)
---
+++ JAVASCRIPT/ex009/ex009.js
@@ -0,0 +1,6 @@
+var país = 'EUA'
+console.log(`Vivendo em ${país}`)
+if (país == 'Brasil'){console.log ('VocÊ é Brasileiro')}
+else{
+    console.log('Você é UM gringo!')
+}
